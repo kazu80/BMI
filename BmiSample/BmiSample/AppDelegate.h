315d10ea9c0eeb6acfadeb90863c31406ca86e41
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-22 20:45:59 +0900

    make Project
    プロジェクト作成

---
+++ BmiSample/BmiSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BmiSample
+//
+//  Created by kazuyoshi kawakami on 13/06/22.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BmiSample/BmiSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BmiSample
+//
+//  Created by kazuyoshi kawakami on 13/06/22.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
