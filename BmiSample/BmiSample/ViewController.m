28201e34f747417a55c42b4984112568603a0f84
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-22 22:19:32 +0900

    Add BMI calc, result color, result sound extention
    ・BMI計算機能
    ・結果に対する背景色変更機能
    ・結果に対する音声案内機能

--- BmiSample/BmiSample/ViewController.m
+++ BmiSample/BmiSample/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField * weightText;
+@property (weak, nonatomic) IBOutlet UITextField * heightText;
+@property (weak, nonatomic) IBOutlet UILabel     * resultLabel;
+@property (strong, nonatomic) AVAudioPlayer      * audioPlayer;
 
 @end
 
@@ -17,13 +23,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)sound:(NSString *)file
+{
+    NSURL * url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:file ofType:@"aif"]];
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    [_audioPlayer play];
+}
+
+- (IBAction)bmiButtonPushed:(id)sender
+{
+    float h = _heightText.text.floatValue / 100;
+    float w = _weightText.text.floatValue;
+    
+    float bmi = w / ( h * h );
+    NSLog(@"bmi = %f", bmi);
+    
+    if ( bmi >= 25 ) {
+        _resultLabel.text = @"太り過ぎ";
+        _resultLabel.backgroundColor = [UIColor redColor];
+        [self sound:@"fat"];
+    } else if ( bmi < 18.5 ) {
+        _resultLabel.text = @"やせすぎ";
+        _resultLabel.backgroundColor = [UIColor yellowColor];
+        [self sound:@"skinny"];
+    } else {
+        _resultLabel.text = @"標準";
+        _resultLabel.backgroundColor = [UIColor greenColor];
+        [self sound:@"normal"];
+    }
 }
 
 @end
